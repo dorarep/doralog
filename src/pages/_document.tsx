de43426572f7ed804d5d34a78094a25cfa4eca81
dorarep <ishikami.naotaka@gmail.com>
2020-09-23 01:43:58 +0900

    feat: add link to rss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,7 @@ export default class extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
+          <link rel="alternate" type="application/rss+xml" title="RSS for blog posts" href="/feeds.xml" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
